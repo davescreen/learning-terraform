d8e16c363399a25916fc1684e2b9ba588c97605a
davescreen <davescreen@gmail.com>
2023-04-19 21:05:16 +0100

    Use module

--- main.tf
+++ main.tf
@@ -14,10 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-    default = true
-}
+module "blog-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
 
+  name = "blog-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -35,7 +46,7 @@ module "blog_sg" {
   version = "4.17.2"
 
   name        = "blog_sg"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.blog-vpc.public_subnets[0]
 
     ingress_rules = ["https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
