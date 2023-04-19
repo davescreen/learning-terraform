1145bbe101ae5700232d527f1f278aa9c2a75f62
davescreen <davescreen@gmail.com>
2023-04-19 20:48:34 +0100

    Use module

--- main.tf
+++ main.tf
@@ -48,24 +48,14 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  description = "..."
   vpc_id      = data.aws_vpc.default.id
 
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+    ingress_rules = ["https-443-tcp"]
+    ingress_cidr_blocks = ["0.0.0.0/0"]
+
+    egress_rules = ["all-all"]
+    egress_cidr_blocks= ["0.0.0.0/0"]
 }
 
 # resource "aws_security_group" "blog" {
